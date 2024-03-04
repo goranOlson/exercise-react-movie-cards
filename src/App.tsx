3a4b9d19a53bf8197ffa19c0ba6861b92a16e8d9
Göran Olson <goran@famolson.se>
2024-03-04 16:41:45 +0100

    created add movie form functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AddMovie } from "./AddMovie";
 export function App() {
   return (
     <>
-      <h1>This is the App component</h1>
+      <h1>Movie Cards</h1>
       <AddMovie />
     </>
   );
